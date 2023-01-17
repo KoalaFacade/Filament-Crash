07b10ee0c04b24b3d83772e012ebbf8ca170389d
pemudakoding <pemudakoding@gmail.com>
2023-01-17 21:26:18 +0800

    fix: injection of script

--- src/FilamentCrashServiceProvider.php
+++ src/FilamentCrashServiceProvider.php
@@ -21,33 +21,35 @@ class FilamentCrashServiceProvider extends PluginServiceProvider
     public function packageBooted(): void
     {
         /** @var string $dueDate */
-        $dueDate = config(key: 'filament-crash.dueDate');
+        $dueDate = config(key: 'filament-crash.due_date');
 
         /** @var string $daysDeadline */
-        $daysDeadline = config(key: 'filament-crash.deadline_after_dueDate');
+        $daysDeadline = config(key: 'filament-crash.deadline_after_due_date');
 
         Filament::registerRenderHook(
             name: 'scripts.end',
             callback: fn () => new HtmlString(html: "
-                (function(){
-                    const dueDate = new Date('{$dueDate}');
-                    const daysDeadline = {$daysDeadline};
-          
-                    const current_date = new Date();
-                    const utc1 = Date.UTC(dueDate.getFullYear(), dueDate.getMonth(), dueDate.getDate());
-                    const utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-                    const days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-                    
-                    if(days > 0) {
-                        const daysLate = daysDeadline-days;
-                        let opacity = (daysLate*100/daysDeadline)/100;
-                            opacity = (opacity < 0) ? 0 : opacity;
-                            opacity = (opacity > 1) ? 1 : opacity;
-                        if(opacity >= 0 && opacity <= 1) {
-                            document.getElementsByTagName('BODY')[0].style.opacity = opacity;
+                <script>
+                    (function(){
+                        const dueDate = new Date('{$dueDate}');
+                        let daysDeadline = {$daysDeadline};
+    
+                        const current_date = new Date();
+                        const utc1 = Date.UTC(dueDate.getFullYear(), dueDate.getMonth(), dueDate.getDate());
+                        const utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+                        const days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+    
+                        if(days > 0) {
+                            const daysLate = daysDeadline-days;
+                            let opacity = (daysLate*100/daysDeadline)/100;
+                                opacity = (opacity < 0) ? 0 : opacity;
+                                opacity = (opacity > 1) ? 1 : opacity;
+                            if(opacity >= 0 && opacity <= 1) {
+                                document.getElementsByTagName('BODY')[0].style.opacity = opacity;
+                            }
                         }
-                    }
-                })()
+                    })()
+                </script>
             "));
     }
 }
